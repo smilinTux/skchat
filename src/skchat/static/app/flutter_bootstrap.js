fc8b84a779dff4c9e424e86c2fd0799d45145388
Lumina <lumina@douno.it>
2026-08-11 15:03:18 -0400

    deploy(app): rebuild web client from skworld-app 1ee0bd25f075
    Built with --base-href /app/ and committed, because src/skchat/static/app
    is tracked and an uncommitted bundle is reverted by the next checkout or pull.

--- src/skchat/static/app/flutter_bootstrap.js
+++ src/skchat/static/app/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1872652374" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1966773219" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
